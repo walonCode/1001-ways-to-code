cf710e23b0c80e32484c831c0a34da00130f71f0
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-09-10 01:32:49 +0000

    fix the password gen in javascript

--- javascript/passwordGen.js
+++ javascript/passwordGen.js
@@ -1,24 +1,22 @@
-function generate(length){
+function generate(length) {
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const symbols = ['@','!','$','%','^','&','*','(', ')','_','+'];
+    const numbers = '0123456789'.split('');
 
-    const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    const symbols = ['@','!','$','%','^','&','*','(', ')','_','+']
-    const numbers = ['1','2','3','4','5','6','7','8','9','0']
+    const allchar = letters.concat(symbols, numbers);
 
-    const allchar = letters + symbols + numbers
-
-
-    //still having an issue with the random value in js
-    let password = ""
-    for(let i =0; i <= length; i++){
-        password += allchar[Math.floor(Math.random() * 2)]
+    let password = "";
+    for(let i = 0; i < length; i++){
+        const value = Math.floor(Math.random() * allchar.length);
+        password += allchar[value];
     }
 
-    return password
+    return password;
 }
 
 function main(){
-    password = generate(10);
-    console.log(password)
+    const password = generate(10);
+    console.log(password);
 }
 
-main()
+main();
